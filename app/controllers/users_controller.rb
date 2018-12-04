fd959ffe1f45793e04c7ec6ad8540a7ab9c75c09
github username <github email address>
2018-12-04 17:20:47 -0500

    Added new and create user paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to users_path
+  end
+
+  # def show
+  #   @user.new = User.new()
+  # end
+  #
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :bio)
+    end
+
 end
